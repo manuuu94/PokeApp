a9d0fdf7791759941e66050d2f62299a09d3d975
manu_ <manu_@LAPTOP-7HTT7HMN>
2025-06-16 20:47:08 -0600

    3rd

--- PokeApp/PokeSearch/views.py
+++ PokeApp/PokeSearch/views.py
@@ -15,6 +15,20 @@ def PokeSearch(request):
     except Exception as e:
         print(e)
 
+class Pokemon():
+    def __init__(self, name,order,hp,atk,spatk,defe,spdef,speed):
+        self.name = name
+        self.order = order
+        self.hp = hp
+        self.atk = atk
+        self.spatk = spatk
+        self.defe = defe
+        self.spdef = spdef
+        self.speed = speed
+    
+    def __str__(self):
+        return f"name: {self.name}, order: {self.order}, HP: {self.hp}, Attack: {self.atk}, Sp.Atk: {self.spatk}, Defense: {self.defense}, Sp.Def: {self.spdefense}, Speed: {self.speed}"   
+
 
 def SearchPokemon(value):
     try:
@@ -23,18 +37,11 @@ def SearchPokemon(value):
         response = requests.get(apiurl)
         if response.status_code == 200:
             data = response.json()
-            name = data["name"]
-            order = data["order"]
-            hp = data["stats"][0]["base_stat"]
-            atk = data["stats"][1]["base_stat"]
-            spatk = data["stats"][2]["base_stat"]
-            defense = data["stats"][3]["base_stat"]
-            spdefense = data["stats"][4]["base_stat"]
-            speed = data["stats"][5]["base_stat"]
-            print(f"name: {name}, order: {order}, HP: {hp}, Attack: {atk}, Sp.Atk: {spatk}, Defense: {defense}, Sp.Def: {spdefense}, Speed: {speed}")
-            dict = {"Name":name,"Order":order,"HP":hp,
-                    "Attack":atk,"Sp.Attack":spatk,"Defense":defense,
-                    "Sp.Defense":spdefense,"Speed":speed}
+            pokemon = NewPokemon(data)
+            dict = {"Name":pokemon.name,"Order":pokemon.order,
+                    "HP":pokemon.hp,"Attack":pokemon.atk,
+                    "Sp.Attack":pokemon.spatk,"Defense":pokemon.defe,
+                    "Sp.Defense":pokemon.spdef,"Speed":pokemon.speed}
             return dict
         elif response.status_code == 404: 
             print("Not found.")
@@ -44,3 +51,15 @@ def SearchPokemon(value):
         print(e)
 
 
+def NewPokemon(data):
+        pokemon = Pokemon
+        pokemon.name = data["name"]
+        pokemon.order = data["order"]
+        pokemon.hp = data["stats"][0]["base_stat"]
+        pokemon.atk = data["stats"][1]["base_stat"]
+        pokemon.spatk = data["stats"][2]["base_stat"]
+        pokemon.defe = data["stats"][3]["base_stat"]
+        pokemon.spdef = data["stats"][4]["base_stat"]
+        pokemon.speed = data["stats"][5]["base_stat"]
+        print(pokemon)
+        return pokemon
